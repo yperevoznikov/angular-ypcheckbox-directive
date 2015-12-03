d508be0b7d1b51f0cfbe5342ca38795ba0015104
Yuriy Perevoznikov <yuriless@gmail.com>
2015-12-03 23:28:45 +0300

    added basic functionality

--- src/ypcheckbox-directive.js
+++ src/ypcheckbox-directive.js
@@ -4,13 +4,60 @@
     angular.module('yperevoznikov.ypcheckbox', [])
 
         .directive('ypcheckbox', function() {
-            function link(scope, el, attrs) {
+            var link = function(scope, el, attrs, ngModelCtrl) {
 
-            }
+                ngModelCtrl.$setViewValue(!!ngModelCtrl.$viewValue);
+
+                var checkEl = angular.element(el.children()[0]);
+                var checkElInner = angular.element(checkEl.children()[0]);
+                var checkElLabel = angular.element(el.children()[1]);
+
+                checkEl.css({
+                    'border': '2px solid red',
+                    'width': '8px',
+                    'height': '8px',
+                    'display': 'inline-block',
+                    'borderRadius': '5px',
+                    'padding': '2px',
+                    'cursor': 'pointer',
+                    'verticalAlign': 'middle',
+                    'marginRight': '5px'
+                });
+
+                checkElInner.css({
+                    'width': '100%',
+                    'height': '100%',
+                    'background': 'red',
+                    'verticalAlign': 'middle',
+                    'borderRadius': '5px',
+                    'display': ngModelCtrl.$viewValue ? 'inline-block' : 'none'
+                });
+
+                checkElLabel.css({'verticalAlign': 'middle'});
+
+                var onClick = function () {
+                    ngModelCtrl.$setViewValue(!ngModelCtrl.$viewValue);
+                    checkElInner.css('display', ngModelCtrl.$viewValue ? 'inline-block' : 'none');
+                };
+                checkEl.bind('click', onClick);
+                checkElLabel.bind('click', onClick);
+
+            };
+
+            var compile = function(tElement, tAttrs, transclude) {
+                return link;
+            };
+
             return {
                 restrict: 'A',
                 require: 'ngModel',
-                link: link
+                transclude: true,
+                compile: compile,
+                template:
+                    '<span class="ypcheckbox-check">' +
+                        '<span class="ypcheckbox-check-inner"></span>' +
+                    '</span>' +
+                    '<span class="ypcheckbox-label"><ng-transclude></ng-transclude></span>'
             };
         })
 
